db33c3a08f7c52f023387f410825d646f70f6126
Rastko Zivkovic <rale.pablito@gmail.com>
2022-02-22 17:50:48 +0100

    added basic material ui grid layout

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { Typography, Button, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import {
+  Typography,
+  Box,
+  Container,
+  Grid,
+  Button,
+  TextField,
+} from "@mui/material";
 import { useFormik } from "formik";
 import * as yup from "yup";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Register = () => {
   const supportedFormats = ["image/jpg", "image/jpeg", "image/png"];
@@ -26,10 +35,16 @@ const Register = () => {
     avatar: yup
       .mixed("Enter picture")
       .required("Image file is required!")
-      .test("fileType", "Only jpg/jpeg/png files are supported!", value =>
-        supportedFormats.includes(value.type)
+      .test(
+        "fileType",
+        "Only jpg/jpeg/png files are supported!",
+        value => value === undefined || supportedFormats.includes(value.type)
       )
-      .test("fileSize", "File is too large", value => value.size < 1_000_000),
+      .test(
+        "fileSize",
+        "File is too large",
+        value => value === undefined || value.size < 1_000_000
+      ),
   });
 
   const formik = useFormik({
@@ -47,67 +62,98 @@ const Register = () => {
   });
 
   return (
-    <>
-      <form onSubmit={formik.handleSubmit}>
-        <Typography>Register</Typography>
-        <TextField
-          id="username"
-          name="username"
-          label="Username"
-          value={formik.values.username}
-          onChange={formik.handleChange}
-          error={formik.touched.username && Boolean(formik.errors.username)}
-          helperText={formik.touched.username && formik.errors.username}
-        />
-        <TextField
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        <TextField
-          id="passwordConfirm"
-          name="passwordConfirm"
-          label="Confirm password"
-          type="password"
-          value={formik.values.passwordConfirm}
-          onChange={formik.handleChange}
-          error={
-            formik.touched.passwordConfirm &&
-            Boolean(formik.errors.passwordConfirm)
-          }
-          helperText={
-            formik.touched.passwordConfirm && formik.errors.passwordConfirm
-          }
-        />
-        <TextField
-          id="avatar"
-          name="avatar"
-          type="file"
-          onChange={e => {
-            formik.values.avatar = e.target.files[0];
-          }}
-          error={formik.touched.avatar && Boolean(formik.errors.avatar)}
-          helperText={formik.touched.avatar && formik.errors.avatar}
-        />
-        <Button variant="contained" type="submit" size="small">
-          Submit
-        </Button>
-      </form>
-    </>
+    <form onSubmit={formik.handleSubmit}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
+      >
+        <Grid item>
+          <InstagramIcon />
+        </Grid>
+
+        <Grid item>
+          <TextField
+            id="username"
+            name="username"
+            label="Username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            error={formik.touched.username && Boolean(formik.errors.username)}
+            helperText={formik.touched.username && formik.errors.username}
+          />
+        </Grid>
+
+        <Grid item>
+          <TextField
+            id="email"
+            name="email"
+            label="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+        </Grid>
+
+        <Grid item>
+          <TextField
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+        </Grid>
+
+        <Grid item>
+          <TextField
+            id="passwordConfirm"
+            name="passwordConfirm"
+            label="Confirm password"
+            type="password"
+            value={formik.values.passwordConfirm}
+            onChange={formik.handleChange}
+            error={
+              formik.touched.passwordConfirm &&
+              Boolean(formik.errors.passwordConfirm)
+            }
+            helperText={
+              formik.touched.passwordConfirm && formik.errors.passwordConfirm
+            }
+          />
+        </Grid>
+
+        <Grid item>
+          <TextField
+            id="avatar"
+            name="avatar"
+            type="file"
+            variant="standard"
+            onChange={e => {
+              formik.values.avatar = e.target.files[0];
+            }}
+            error={formik.touched.avatar && Boolean(formik.errors.avatar)}
+            helperText={formik.touched.avatar && formik.errors.avatar}
+          />
+        </Grid>
+
+        <Grid item>
+          <Button variant="outlined" type="submit" size="small">
+            Register
+          </Button>
+        </Grid>
+
+        <Grid item>
+          <Link to={"/login"}>Already have an account? Log in!</Link>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
