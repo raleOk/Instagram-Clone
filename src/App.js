ae8677b1bfdbe7ca89b5913d4d6e1b8d3109187f
Rastko Zivkovic <rale.pablito@gmail.com>
2022-03-10 18:49:59 +0100

    added routes and refactored

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ import CreatePost from "./pages/NavigationTabs/CreatePost";
 import LikedPosts from "./pages/NavigationTabs/LikedPosts";
 import MyPosts from "./pages/NavigationTabs/MyPosts";
 import Profile from "./pages/NavigationTabs/Profile";
+import Settings from "./pages/NavigationTabs/Settings/Settings";
+import ChangeUsername from "./pages/NavigationTabs/Settings/ChangeUsername";
+import ChangeAvatar from "./pages/NavigationTabs/Settings/ChangeAvatar";
+import ChangePassword from "./pages/NavigationTabs/Settings/ChangePassword";
+import DeleteAccount from "./pages/NavigationTabs/Settings/DeleteAccount";
 import { authContext } from "./auth/useAuth";
 
 const App = () => {
@@ -26,18 +31,24 @@ const App = () => {
       <Route path="/" element={<ProtectedRoutes />}>
         <Route path="/" element={<Navbar />} exact>
           <Route path="/" element={<Home />} exact />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/liked" element={<LikedPosts />} />
-          <Route path="/my-posts" element={<MyPosts />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="create" element={<CreatePost />} />
+          <Route path="liked" element={<LikedPosts />} />
+          <Route path="my-posts" element={<MyPosts />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />}>
+            <Route path="change-username" element={<ChangeUsername />} />
+            <Route path="change-avatar" element={<ChangeAvatar />} />
+            <Route path="change-password" element={<ChangePassword />} />
+            <Route path="delete-account" element={<DeleteAccount />} />
+          </Route>
         </Route>
       </Route>
       <Route path="/" element={<UnprotectedRoutes />}>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/verify" element={<Verify />} />
-        <Route path="/forgot" element={<Forgot />} />
-        <Route path="/reset" element={<Reset />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="verify" element={<Verify />} />
+        <Route path="forgot" element={<Forgot />} />
+        <Route path="reset" element={<Reset />} />
       </Route>
       <Route path="/*" element={<NotFound />} />
     </Routes>
