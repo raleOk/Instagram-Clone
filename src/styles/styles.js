67251a3097d94ede09aa905fffcfba9f8214de1e
Rastko Zivkovic <rale.pablito@gmail.com>
2022-03-16 22:38:48 +0100

    modal styles and small import fix

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,4 +2,16 @@ const errorStyles = {
   sx: { width: 180 },
 };
 
-export { errorStyles };
+const deleteModalStyles = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export { errorStyles, deleteModalStyles };
