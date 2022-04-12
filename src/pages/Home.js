279afcbc379c1b1707b932aba3f9d332466b5f8f
Rastko Zivkovic <rale.pablito@gmail.com>
2022-04-12 20:46:15 +0200

    home page posts list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Grid } from "@mui/material";
+import Loader from "../components/Loaders/Loader";
+import Post from "../components/Posts/Post";
+import { getAllPosts } from "../api/api";
 
 const Home = () => {
-  return <div>home</div>;
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setIsLoading(true);
+      const response = await getAllPosts();
+      const data = response.data.posts;
+      setPosts(data);
+      setIsLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  const postsList = (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={3}
+      sx={{ mt: 3 }}
+    >
+      {posts.map(post => {
+        return (
+          <Grid item key={post._id}>
+            <Post
+              avatar={post.user.avatar}
+              username={post.user.username}
+              createdAt={post.createdAt}
+              media={post.media}
+              caption={post.caption}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+
+  return <>{isLoading ? <Loader /> : postsList}</>;
 };
 
 export default Home;
